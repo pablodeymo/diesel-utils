b1148c99bbddd1ec1f43b2b06db601d7d05e6e9d
Pablo Deymonnaz <deymonnaz@gmail.com>
2020-06-22 14:50:34 -0300

    Added lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+#![warn(warnings, rust_2018_idioms, unsafe_code)]
 #[macro_use]
 extern crate log;
 #[macro_use]
 extern crate diesel;
 #[macro_use]
 extern crate diesel_migrations;
-extern crate dotenv;
 
 pub mod connection;
 pub mod paginate;
